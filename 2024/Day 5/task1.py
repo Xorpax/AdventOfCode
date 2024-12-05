adce4f9745796904ecde77944b93b2bf239a9d4f
Xorpax <petarda795@gmail.com>
2024-12-05 13:31:29 +0000

    solve task 1

---
+++ 2024/Day 5/task1.py
@@ -0,0 +1,28 @@
+import math
+import time
+
+def format_data(file_path: str) -> tuple[list[int], list[int]]:
+    with open(file_path, "r") as data:
+        lines = data.readlines()
+        order = [tuple(map(int, line.strip().split("|"))) for line in lines if "|" in line]
+        updates = [tuple(map(int, line.strip().split(","))) for line in lines if "," in line]
+        return order, updates
+    
+def is_in_order(update, order):
+    return all((p1, p2) in order for p1, p2 in zip(update, update[1:]))
+
+if __name__ == "__main__":
+    start = time.time()
+    file_path = r".\2024\Day 5\input1.txt"
+    # file_path = r".\2024\Day 5\test.txt"
+    order, updates = format_data(file_path)
+    total = 0
+    for update in updates:
+        if is_in_order(update, order):
+            l_update = len(update)
+            total += update[math.ceil(l_update / 2 ) - 1]
+
+    print(total)
+    end = time.time()
+    print(f"{(end - start) * 1000:.4f}ms")
+    # answer is 4689
