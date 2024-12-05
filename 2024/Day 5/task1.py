dff8b5ea7ced2b4647f898b84447849c55c3ef78
Xorpax <petarda795@gmail.com>
2024-12-05 13:43:02 +0000

    update file path

--- 2024/Day 5/task1.py
+++ 2024/Day 5/task1.py
@@ -11,9 +11,16 @@ def format_data(file_path: str) -> tuple[list[int], list[int]]:
 def is_in_order(update, order):
     return all((p1, p2) in order for p1, p2 in zip(update, update[1:]))
 
+def order(update, order):
+    temp = update[:]
+    for p1, p2 in zip(temp, temp[1:]):
+        if (p1, p2) not in order:
+            p1, p2 = p2, p1
+
+
 if __name__ == "__main__":
     start = time.time()
-    file_path = r".\2024\Day 5\input1.txt"
+    file_path = r".\2024\Day 5\input.txt"
     # file_path = r".\2024\Day 5\test.txt"
     order, updates = format_data(file_path)
     total = 0
